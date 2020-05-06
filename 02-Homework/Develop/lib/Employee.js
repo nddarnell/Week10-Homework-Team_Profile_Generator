5a8b2cbcb6117f9aca2461320f26d7c0fc2704e2
nddarnell_Desktop <60324766+nddarnell@users.noreply.github.com>
2020-05-05 17:29:09 -0700

    Added employee and engineer classes

--- 02-Homework/Develop/lib/Employee.js
+++ 02-Homework/Develop/lib/Employee.js
@@ -1,1 +1,27 @@
 // TODO: Write code to define and export the Employee class
+
+function Employee(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.role = "Employee";
+  }
+  
+  Employee.prototype.getName = function getName() {
+    return this.name;
+  };
+    
+  Employee.prototype.getId = function getId() {
+    return this.id;
+  };
+    
+  Employee.prototype.getEmail = function getEmail() {
+    return this.email;
+  };
+    
+  Employee.prototype.getRole = function getRole() {
+    return this.role;
+  };
+  
+  module.exports = Employee;
+  
--- 02-Homework/Develop/lib/Engineer.js
+++ 02-Homework/Develop/lib/Engineer.js
@@ -1,1 +1,16 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+
+function Engineer(name, id, email, github) {
+    Employee.call(this, name, id, email)
+    this.role = "Engineer";
+    this.github = github;
+  }
+  Engineer.prototype = Object.create(Employee.prototype)
+    
+  Engineer.prototype.getGithub = function getGithub() {
+    return this.github;
+  };
+  
+
+  module.exports = Engineer;
