a2e511a40bc5e9aa4dc9dcfdeecb16ea0626cd67
nddarnell_Desktop <60324766+nddarnell@users.noreply.github.com>
2020-05-05 18:21:25 -0700

    Added manager class function

--- 02-Homework/Develop/lib/Manager.js
+++ 02-Homework/Develop/lib/Manager.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+
+function Manager(name, id, email, officeNumber) {
+    Employee.call(this, name, id, email)
+    this.role = "Manager";
+    this.officeNumber = officeNumber;
+  }
+  Manager.prototype = Object.create(Employee.prototype)
+    
+  Manager.prototype.getOfficeNumber = function getOfficeNumber() {
+    return this.officeNumber;
+  };
+  
+  module.exports = Manager;
