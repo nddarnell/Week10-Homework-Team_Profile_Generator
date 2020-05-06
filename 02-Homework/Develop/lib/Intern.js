44077f6b7aa540b644c8721ef20edf41c57dcb81
nddarnell_Desktop <60324766+nddarnell@users.noreply.github.com>
2020-05-05 21:43:21 -0700

    Added intern class in intern.js

--- 02-Homework/Develop/lib/Intern.js
+++ 02-Homework/Develop/lib/Intern.js
@@ -1,1 +1,15 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+
+function Intern(name, id, email, school) {
+    Employee.call(this, name, id, email)
+    this.role = "Intern";
+    this.school = school;
+  }
+  Intern.prototype = Object.create(Employee.prototype)
+    
+  Intern.prototype.getSchool = function getSchool() {
+    return this.school;
+  };
+  
+  module.exports = Intern;
